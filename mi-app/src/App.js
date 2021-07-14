320aad9aef0bea44207470e206a957cad961b9e9
LeyBatenkaitos <gbhr115@gmail.com>
2021-07-14 17:50:58 -0400

    tarea: Primer React con JSX

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import React, { Component } from "react";
+import React from "react";
 import Home from "./componentes/home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hola</h1>
+    <div>
+      <h1>Hola Dojo!</h1>
       <div>
         <Home />
       </div>
--- mi-app/src/componentes/home.jsx
+++ mi-app/src/componentes/home.jsx
@@ -4,12 +4,20 @@ class Home extends Component {
   render() {
     const msg = "Bienvenido :3";
 
-    const bg = "red";
     console.log("hola soy las props", this.props);
     return (
       <div className="container">
-        <div style={{ background: bg }}>This is our first class component.</div>
-        <div>Hola {msg}</div>
+        <div>This is our first class component.</div>
+        <div>Hola Dojo {msg}</div>
+        <div>
+          <h2>Cosas que necesito hacer:</h2>
+          <ul>
+            <li>Aprender React</li>
+            <li>Escalar el Monte Everest</li>
+            <li>Correr una Maraton</li>
+            <li>Alimentar los perros</li>
+          </ul>
+        </div>
       </div>
     );
   }
