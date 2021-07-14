4d0935c8a28adadc2cc655aca8f85bf721924169
LeyBatenkaitos <gbhr115@gmail.com>
2021-07-13 22:05:58 -0400

    Anexo de React.app

---
+++ mi-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React, { Component } from "react";
+import Home from "./componentes/home";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hola</h1>
+      <div>
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
